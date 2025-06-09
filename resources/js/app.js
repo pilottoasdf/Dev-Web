e770c18e751cd9a4d6bf50c7a9301fd7490f8c23
arend140 <brunoarend01@gmail.com>
2025-06-08 23:57:28 -0300

    Criação de quizzes
    Quizzes podem ser criados e salvos com sucesso no banco de dados

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,9 @@
 import './bootstrap';
 import { createApp } from 'vue';
 import Menu from './components/Menu.vue';
+import CriaPergunta from './components/CriaPergunta.vue';
 
 const app = createApp({});
 app.component('menu-component', Menu)
+app.component('criapergunta-component', CriaPergunta)
 app.mount('#app');
