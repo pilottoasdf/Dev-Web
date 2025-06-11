4870af03bcc114a4551c2994f06e277fa9cb39e9
arend140 <brunoarend01@gmail.com>
2025-06-10 22:17:52 -0300

    Realizar quizzes concluído
    Agora tem como jogar os quizzes sem problemas

--- resources/js/components/CarregaQuiz.vue
+++ resources/js/components/CarregaQuiz.vue
@@ -5,21 +5,48 @@
     <button type="button" @click="iniciarQuiz">Iniciar Quiz</button>
   </div>
 
-  <div v-if="valor>0">
+  <div v-if="valor>0 && valor<=perguntas.length">
     <h2>{{ perguntas[valor-1].pergunta }}</h2>
-    <label for="alternativa_1_"></label>
-    <input type="radio" :v-name="'alternativa_1_' + (valor-1)">
+    <button :style="{
+    backgroundColor:
+      correta == 1 && etapa === 1
+        ? 'green'
+        : (correta!=1 && etapa === 1 
+            ? 'silver' : 
+            (selecionada === 1) ? 'lightblue' : '')
+  }" @click="selecionaAlternativa(1)" type="button">{{ perguntas[valor-1].alternativas[0] }}</button>
+
+    <button :style="{
+    backgroundColor:
+      correta == 2 && etapa === 1
+        ? 'green'
+        : (correta!=2 && etapa === 1 
+            ? 'silver' : 
+            (selecionada === 2) ? 'lightblue' : '')
+  }" @click="selecionaAlternativa(2)" type="button">{{ perguntas[valor-1].alternativas[1] }}</button>
+    <button :style="{
+    backgroundColor:
+      correta == 3 && etapa === 1
+        ? 'green'
+        : (correta!=3 && etapa === 1 
+            ? 'silver' : 
+            (selecionada === 3) ? 'lightblue' : '')
+  }" @click="selecionaAlternativa(3)" type="button">{{ perguntas[valor-1].alternativas[2] }}</button>
+    <button :style="{
+    backgroundColor:
+      correta == 4 && etapa === 1
+        ? 'green'
+        : (correta!=4 && etapa === 1 
+            ? 'silver' : 
+            (selecionada === 4) ? 'lightblue' : '')
+  }" @click="selecionaAlternativa(4)" type="button">{{ perguntas[valor-1].alternativas[3] }}</button>
+
     <br>
-    <label for="alternativa_1_"></label>
-    <input type="radio" :v-name="'alternativa_2_' + (valor-1)">
-    <br>
-    <label for="alternativa_1_"></label>
-    <input type="radio" :v-name="'alternativa_3_' + (valor-1)">
-    <br>
-    <label for="alternativa_1_"></label>
-    <input type="radio" :v-name="'alternativa_4_' + (valor-1)">
-    <br>
+    <button @click="avancaPergunta" type="button">Avançar</button>
     
+  </div>
+  <div v-if="valor>perguntas.length">
+    <h2>Você acertou {{ acertos }} questões de {{ perguntas.length }}</h2>
   </div>
 
 </template>
@@ -38,13 +65,40 @@ export default {
     return {
       quiz: quizObj,
       perguntas: JSON.parse(quizObj.perguntas),
-      valor: 0
+      valor: 0,
+
+      correta: 0,
+      selecionada: 0,
+
+      etapa: 0,
+      
+      acertos: 0,
     }
   },
   methods:{
     iniciarQuiz(){
       this.valor = 1
     },
+    selecionaAlternativa(numeroAlternativa){
+      if(this.etapa===0){
+        this.correta = this.perguntas[(this.valor)-1].alternativa_correta
+        this.selecionada = numeroAlternativa
+      }
+    },
+    avancaPergunta(){
+      if(this.selecionada!=0){
+        if(this.etapa===0){
+          this.etapa = 1
+        }else{
+          if(this.correta==this.selecionada){
+            this.acertos++
+          }
+          this.valor++
+          this.selecionada = 0
+          this.etapa = 0
+        }
+      }
+    }
   }
 }
 </script>
