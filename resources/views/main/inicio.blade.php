87284f5a46e92961fa0b91d75d308d9c529db005
Kauabasso <kauabasso8@gmail.com>
2025-06-10 22:24:16 -0300

    tela inicial completa

--- resources/views/main/inicio.blade.php
+++ resources/views/main/inicio.blade.php
@@ -256,6 +256,7 @@
   </style>
 </head>
 <body>
+<<<<<<< Updated upstream
   <div class="perfil">
     <div class="bolinha"></div>
     <div class="info">
@@ -268,12 +269,65 @@
       <a href="{{ route('forcar-logout') }}">Clica aqui pra deslogar</a>
     </div>
   </div>
+=======
+<!--inicio do menu-->
+<div id="app">
+ <nav class="navbar tamanho-menu navbar-dark ">
+  <div class="container-fluid px-0 d-flex justify-content-between align-items-center" style="width: 100vw;">
+
+    
+    <div class="ms-3">
+      <a class="navbar-brand" href="#">
+        <img class="logo" src="{{ asset('logo.png') }}" alt="logo do site" >
+      </a>
+    </div>
+
+    
+    <div class="position-absolute start-50 translate-middle-x d-flex gap-3">
+      <a href="/inicio"><button class="btn botao-menu btn-light">Página Inicial</button></a>
+      <a href="#"><button class="btn botao-menu btn-light">Games</button></a>
+      <a href="/quizzes"><button class="btn botao-menu btn-light">Quizzes</button></a>
+    </div>
+
+<div class="me-3 dropdown d-flex align-items-center">
+  <img class="usuario me-2" src="https://cdn-icons-png.flaticon.com/512/303/303593.png" alt="imagem do usuario" style="width: 40px; height: 40px; border-radius: 50%;">
+  
+  <button 
+    class="btn btn-dark dropdown-toggle" 
+    type="button" 
+    data-bs-toggle="dropdown" 
+    aria-expanded="false"
+  >
+    Perfil
+  </button>
+  
+  <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end p-3" style="min-width: 200px;">
+    <li><span class="jogando-como">Jogando como</span></li>
+    <li><strong>{{ auth()->user()->name }}</strong></li>
+    <li><small>{{ auth()->user()->email }}</small></li>
+    <li><hr class="dropdown-divider"></li>
+    <li>
+      <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button type="submit" class="dropdown-item btn btn-link text-start">Sair</button>
+      </form>
+    </li>
+  </ul>
+</div>
+>>>>>>> Stashed changes
 
   <div class="menu-superior">
     <button>Página Inicial</button>
     <button>Games</button>
     <button>Quizzes</button>
   </div>
+
+
+  </div>
+</nav>
+
+</div>
+<!--fim do menu-->
 
 
   <div class="banner" id="banner">
@@ -291,6 +345,7 @@
     Recomendados para você:
   </div>
 
+<<<<<<< Updated upstream
 
   <div class="cards-container">
     <div class="card">
@@ -314,8 +369,127 @@
 
 
     <div class="card adicionar">+</div>
-  </div>
+=======
+ <div class="container my-4" style="max-width: 1140px;">
+  <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+    <div class="d-flex justify-content-center align-items-center">
 
+      <!-- Botão-->
+      <button class="btn botao-carrossel me-2" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Anterior</span>
+      </button>
+
+      <!-- Slides -->
+      <div class="carousel-inner">
+
+        <!-- Slide 1 -->
+        <div class="carousel-item active">
+          <div class="cards-wrapper">
+            <div class="cards-row d-flex flex-row flex-nowrap gap-3 px-3">
+              <!-- Card 1 -->
+              <div class="card d-flex flex-row overflow-hidden" style="width: 350px; height: 140px;">
+                <div class="imagem_card">
+                  <img src="https://blog.ebaconline.com.br/blog/wp-content/uploads/2022/12/kcnb4lkkucfipej3ngqxm-scaled-e1674744655926.jpeg" alt="Imagem" style="width: 100%; height: 100%; object-fit: cover;">
+                </div>
+                <div class="p-3 texto-card">
+                  <h5 class="card-title">Jogo 1</h5>
+                  <p class="card-text">Texto do jogo 1</p>
+                  <p class="card-text"><small class="text-muted">Matemática – Análise Combinatória</small></p>
+                </div>
+              </div>
+
+              <!-- Card 2 -->
+              <div class="card d-flex flex-row overflow-hidden" style="width: 350px; height: 140px;">
+                <div class="imagem_card">
+                  <img src="https://blog.ebaconline.com.br/blog/wp-content/uploads/2022/12/kcnb4lkkucfipej3ngqxm-scaled-e1674744655926.jpeg" alt="Imagem" style="width: 100%; height: 100%; object-fit: cover;">
+                </div>
+                <div class="p-3 texto-card">
+                  <h5 class="card-title">Quiz 1</h5>
+                  <p class="card-text">Texto do jogo 1</p>
+                  <p class="card-text"><small class="text-muted">História – Pré-História</small></p>
+                </div>
+              </div>
+
+              <!-- Card 3 -->
+              <div class="card d-flex flex-row overflow-hidden" style="width: 350px; height: 140px;">
+                <div class="imagem_card">
+                  <img src="https://blog.ebaconline.com.br/blog/wp-content/uploads/2022/12/kcnb4lkkucfipej3ngqxm-scaled-e1674744655926.jpeg" alt="Imagem" style="width: 100%; height: 100%; object-fit: cover;">
+                </div>
+                <div class="p-3 texto-card">
+                  <h5 class="card-title">Quiz 2</h5>
+                  <p class="card-text">Texto do jogo 2</p>
+                  <p class="card-text"><small class="text-muted">História – Antiguidade Clássica</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- Slide 2 -->
+        <div class="carousel-item">
+          <div class="cards-wrapper">
+            <div class="cards-row d-flex flex-row flex-nowrap gap-3 px-3">
+              <!-- Card 4 -->
+              <div class="card d-flex flex-row overflow-hidden" style="width: 350px; height: 140px;">
+                <div class="imagem_card">
+                  <img src="https://blog.ebaconline.com.br/blog/wp-content/uploads/2022/12/kcnb4lkkucfipej3ngqxm-scaled-e1674744655926.jpeg" alt="Imagem" style="width: 100%; height: 100%; object-fit: cover;">
+                </div>
+                <div class="p-3 texto-card">
+                  <h5 class="card-title">Quiz 3</h5>
+                  <p class="card-text">Outro exemplo</p>
+                  <p class="card-text"><small class="text-muted">Ciências – Sistema Solar</small></p>
+                </div>
+              </div>
+
+              <!-- Card 5 -->
+              <div class="card d-flex flex-row overflow-hidden" style="width: 350px; height: 140px;">
+                <div class="imagem_card">
+                  <img src="https://blog.ebaconline.com.br/blog/wp-content/uploads/2022/12/kcnb4lkkucfipej3ngqxm-scaled-e1674744655926.jpeg" alt="Imagem" style="width: 100%; height: 100%; object-fit: cover;">
+                </div>
+                <div class="p-3 texto-card">
+                  <h5 class="card-title">Quiz 4</h5>
+                  <p class="card-text">Outro exemplo</p>
+                  <p class="card-text"><small class="text-muted">Ciências – Sistema Solar</small></p>
+                </div>
+              </div>
+
+              <!-- Card 6 -->
+              <div class="card d-flex flex-row overflow-hidden" style="width: 350px; height: 140px;">
+                <div class="imagem_card">
+                  <img src="https://blog.ebaconline.com.br/blog/wp-content/uploads/2022/12/kcnb4lkkucfipej3ngqxm-scaled-e1674744655926.jpeg" alt="Imagem" style="width: 100%; height: 100%; object-fit: cover;">
+                </div>
+                <div class="p-3 texto-card">
+                  <h5 class="card-title">Quiz 5</h5>
+                  <p class="card-text">Outro exemplo</p>
+                  <p class="card-text"><small class="text-muted">Ciências – Sistema Solar</small></p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div> <!-- Fim do carrossel-->
+
+      <!-- Botão-->
+      <button class="btn botao-carrossel ms-2" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Próximo</span>
+      </button>
+    </div> 
+>>>>>>> Stashed changes
+  </div>
+</div>
+
+
+
+  <!-- Card adicionar-->
+ <div class="card adicionar mx-auto" style="max-width: 100%; width: 1050px; height: 40px;">Ver mais...</div>
+
+<<<<<<< Updated upstream
+=======
+</div>
+>>>>>>> Stashed changes
 
   <script>
    
