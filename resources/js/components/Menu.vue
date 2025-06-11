122278c979f81515ca43178e4b63a24065830cdc
arend140 <brunoarend01@gmail.com>
2025-06-10 23:43:56 -0300

    CSS do Kauã e Menu arrumado

--- resources/js/components/Menu.vue
+++ resources/js/components/Menu.vue
@@ -1,21 +1,53 @@
 <template>
-  <div class="perfil">
-    <div class="bolinha"></div>
-    <div class="info">
-      <span class="jogando-como">Jogando como</span>
-      {{ userName }}
-      <br>
-      {{ userEmail }}
-      <br>
-      <a href="/logout">Clica aqui pra deslogar</a>
-    </div>
-  </div>
+ <nav class="navbar tamanho-menu navbar-dark ">
+  <div class="container-fluid px-0 d-flex justify-content-between align-items-center" style="width: 100vw;">
 
-  <div class="menu-superior">
-    <a href="/inicio"><button>Página Inicial</button></a>
-    <button>Games</button>
-    <a href="/quizzes"><button>Quizzes</button></a>
+
+   
+    <div class="ms-3">
+      <a class="navbar-brand" href="/inicio">
+        <img class="logo" src="/public/logo.png" alt="logo do site" >
+      </a>
+    </div>
+
+
+   
+    <div class="position-absolute start-50 translate-middle-x d-flex gap-3">
+      <a href="/inicio"><button class="btn botao-menu btn-light">Página Inicial</button></a>
+      <a href="#"><button class="btn botao-menu btn-light">Games</button></a>
+      <a href="/quizzes"><button class="btn botao-menu btn-light">Quizzes</button></a>
+    </div>
+
+
+<div class="me-3 dropdown d-flex align-items-center">
+  <img class="usuario me-2" src="https://cdn-icons-png.flaticon.com/512/303/303593.png" alt="imagem do usuario" style="width: 40px; height: 40px; border-radius: 50%;">
+ 
+  <button
+    class="btn btn-dark dropdown-toggle"
+    type="button"
+    data-bs-toggle="dropdown"
+    aria-expanded="false"
+  >
+    Perfil
+  </button>
+ 
+  <ul class="dropdown-menu dropdown-menu-dark dropdown-menu-end p-3" style="min-width: 200px;">
+    <li><span class="jogando-como">Jogando como</span></li>
+    <li><strong>{{ userName }}</strong></li>
+    <li><small>{{ userEmail }}</small></li>
+    <li><hr class="dropdown-divider"></li>
+    <li>
+      <a href="/meus-projetos" type="submit" class="dropdown-item btn btn-link text-start">Meus Projetos</a>
+    </li>
+    <li>
+      <a href="/logout" type="submit" class="dropdown-item btn btn-link text-start">Sair</a>
+    </li>
+  </ul>
+</div>
   </div>
+</nav>
+
+
 </template>
 
 <script>
