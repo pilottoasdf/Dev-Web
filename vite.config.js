0b271adbf0beefa7a2a8fec4ce18cf5dc244632b
ana_pilotto <anaamalia.cenci@gmail.com>
2025-08-15 00:22:07 -0300

    correção vite

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,10 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/css/cadastro3.css', 'resources/css/carregarQuiz.css', 'resources/css/editar.css', 'resources/css/form.css', 'resources/css/info.css', 'resources/css/inicio.css', 'resources/css/login2.css', 'resources/css/perfil.css', 'resources/css/preferencia.css', 'resources/css/quizzes.css', 'resources/css/ranking.css', 'resources/js/app.js', 'resources/js/components/CarregaQuiz.vue', 'resources/js/components/CriaPergunta.vue', 'resources/js/components/ListagemQuiz.vue', 'resources/js/components/Menu.vue'],
+            input: ['resources/css/app.css', 'resources/css/cadastro3.css', 'resources/css/carregarQuiz.css', 'resources/css/editar.css', 'resources/css/form.css', 'resources/css/info.css', 'resources/css/inicio.css', 'resources/css/login2.css', 'resources/css/perfil.css', 'resources/css/preferencia.css', 'resources/css/quizzes.css', 'resources/css/ranking.css', 'resources/js/app.js', 'resources/js/components/CarregaQuiz.vue', 'resources/js/components/CriaPergunta.vue', 'resources/js/components/ListagemQuiz.vue', 'resources/js/components/Menu.vue', 'resources/js/inicio.js'],
             refresh: true,
         }),
-        vue(),
+        vue(), 
         tailwindcss(),
     ],
 });
